1855ca3fbcb169b4808277e3f60df2f2d83affd3
Allison Shi <shiae@rose-hulman.edu>
2021-01-07 17:32:59 -0500

    Updates to work on pi

---
+++ SDRcode/top_block.py
@@ -0,0 +1,201 @@
+#!/usr/bin/env python2
+# -*- coding: utf-8 -*-
+##################################################
+# GNU Radio Python Flow Graph
+# Title: Top Block
+# Generated: Fri Dec 18 01:10:44 2020
+##################################################
+
+from distutils.version import StrictVersion
+
+if __name__ == '__main__':
+    import ctypes
+    import sys
+    if sys.platform.startswith('linux'):
+        try:
+            x11 = ctypes.cdll.LoadLibrary('libX11.so')
+            x11.XInitThreads()
+        except:
+            print "Warning: failed to XInitThreads()"
+
+from PyQt5 import Qt, QtCore
+from gnuradio import blocks
+from gnuradio import eng_notation
+from gnuradio import filter
+from gnuradio import gr
+from gnuradio.eng_option import eng_option
+from gnuradio.filter import firdes
+from optparse import OptionParser
+import osmosdr
+import pmt
+import sys
+import time
+from gnuradio import qtgui
+
+
+class top_block(gr.top_block, Qt.QWidget):
+
+    def __init__(self):
+        gr.top_block.__init__(self, "Top Block")
+        Qt.QWidget.__init__(self)
+        self.setWindowTitle("Top Block")
+        qtgui.util.check_set_qss()
+        try:
+            self.setWindowIcon(Qt.QIcon.fromTheme('gnuradio-grc'))
+        except:
+            pass
+        self.top_scroll_layout = Qt.QVBoxLayout()
+        self.setLayout(self.top_scroll_layout)
+        self.top_scroll = Qt.QScrollArea()
+        self.top_scroll.setFrameStyle(Qt.QFrame.NoFrame)
+        self.top_scroll_layout.addWidget(self.top_scroll)
+        self.top_scroll.setWidgetResizable(True)
+        self.top_widget = Qt.QWidget()
+        self.top_scroll.setWidget(self.top_widget)
+        self.top_layout = Qt.QVBoxLayout(self.top_widget)
+        self.top_grid_layout = Qt.QGridLayout()
+        self.top_layout.addLayout(self.top_grid_layout)
+
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.restoreGeometry(self.settings.value("geometry", type=QtCore.QByteArray))
+
+
+        ##################################################
+        # Variables
+        ##################################################
+        self.samp_rate = samp_rate = 2e6
+        self.bit_rate = bit_rate = 8
+        self.sensitivity = sensitivity = 2*3.14*1e5
+        self.rf_gain = rf_gain = 60
+        self.repeat = repeat = int(samp_rate/bit_rate)
+        self.packet_size = packet_size = 8
+        self.low_freq = low_freq = 50e3
+        self.freq_spread = freq_spread = 50e3
+        self.carrier_freq = carrier_freq = 2.49e9
+
+        ##################################################
+        # Blocks
+        ##################################################
+        self.osmosdr_sink_1 = osmosdr.sink( args="numchan=" + str(1) + " " + 'bladerf[=0|000287aa5fdd4950816aa7337eb066f4]' )
+        self.osmosdr_sink_1.set_sample_rate(samp_rate)
+        self.osmosdr_sink_1.set_center_freq(carrier_freq, 0)
+        self.osmosdr_sink_1.set_freq_corr(0, 0)
+        self.osmosdr_sink_1.set_gain(rf_gain, 0)
+        self.osmosdr_sink_1.set_if_gain(20, 0)
+        self.osmosdr_sink_1.set_bb_gain(40, 0)
+        self.osmosdr_sink_1.set_antenna('TX1', 0)
+        self.osmosdr_sink_1.set_bandwidth(300e3, 0)
+
+        self.low_pass_filter_0 = filter.fir_filter_fff(1, firdes.low_pass(
+        	1, samp_rate, 2e3, 10e3, firdes.WIN_HAMMING, 6.76))
+        self.blocks_vco_c_0 = blocks.vco_c(samp_rate, sensitivity, 1)
+        self.blocks_unpack_k_bits_bb_0 = blocks.unpack_k_bits_bb(packet_size)
+        self.blocks_uchar_to_float_0 = blocks.uchar_to_float()
+        self.blocks_repeat_0 = blocks.repeat(gr.sizeof_char*1, repeat)
+        self.blocks_multiply_const_vxx_0 = blocks.multiply_const_vff((2*3.14*freq_spread/sensitivity, ))
+        self.blocks_file_source_0 = blocks.file_source(gr.sizeof_char*1, '/home/pi/OBATS/SDRcode/OBATStest.txt', True)
+        self.blocks_file_source_0.set_begin_tag(pmt.PMT_NIL)
+        self.blocks_add_const_vxx_0 = blocks.add_const_vff((2*3.14*low_freq/sensitivity, ))
+
+
+
+        ##################################################
+        # Connections
+        ##################################################
+        self.connect((self.blocks_add_const_vxx_0, 0), (self.blocks_vco_c_0, 0))
+        self.connect((self.blocks_file_source_0, 0), (self.blocks_unpack_k_bits_bb_0, 0))
+        self.connect((self.blocks_multiply_const_vxx_0, 0), (self.blocks_add_const_vxx_0, 0))
+        self.connect((self.blocks_repeat_0, 0), (self.blocks_uchar_to_float_0, 0))
+        self.connect((self.blocks_uchar_to_float_0, 0), (self.low_pass_filter_0, 0))
+        self.connect((self.blocks_unpack_k_bits_bb_0, 0), (self.blocks_repeat_0, 0))
+        self.connect((self.blocks_vco_c_0, 0), (self.osmosdr_sink_1, 0))
+        self.connect((self.low_pass_filter_0, 0), (self.blocks_multiply_const_vxx_0, 0))
+
+    def closeEvent(self, event):
+        self.settings = Qt.QSettings("GNU Radio", "top_block")
+        self.settings.setValue("geometry", self.saveGeometry())
+        event.accept()
+
+    def get_samp_rate(self):
+        return self.samp_rate
+
+    def set_samp_rate(self, samp_rate):
+        self.samp_rate = samp_rate
+        self.set_repeat(int(self.samp_rate/self.bit_rate))
+        self.osmosdr_sink_1.set_sample_rate(self.samp_rate)
+        self.low_pass_filter_0.set_taps(firdes.low_pass(1, self.samp_rate, 2e3, 10e3, firdes.WIN_HAMMING, 6.76))
+
+    def get_bit_rate(self):
+        return self.bit_rate
+
+    def set_bit_rate(self, bit_rate):
+        self.bit_rate = bit_rate
+        self.set_repeat(int(self.samp_rate/self.bit_rate))
+
+    def get_sensitivity(self):
+        return self.sensitivity
+
+    def set_sensitivity(self, sensitivity):
+        self.sensitivity = sensitivity
+        self.blocks_multiply_const_vxx_0.set_k((2*3.14*self.freq_spread/self.sensitivity, ))
+        self.blocks_add_const_vxx_0.set_k((2*3.14*self.low_freq/self.sensitivity, ))
+
+    def get_rf_gain(self):
+        return self.rf_gain
+
+    def set_rf_gain(self, rf_gain):
+        self.rf_gain = rf_gain
+        self.osmosdr_sink_1.set_gain(self.rf_gain, 0)
+
+    def get_repeat(self):
+        return self.repeat
+
+    def set_repeat(self, repeat):
+        self.repeat = repeat
+        self.blocks_repeat_0.set_interpolation(self.repeat)
+
+    def get_packet_size(self):
+        return self.packet_size
+
+    def set_packet_size(self, packet_size):
+        self.packet_size = packet_size
+
+    def get_low_freq(self):
+        return self.low_freq
+
+    def set_low_freq(self, low_freq):
+        self.low_freq = low_freq
+        self.blocks_add_const_vxx_0.set_k((2*3.14*self.low_freq/self.sensitivity, ))
+
+    def get_freq_spread(self):
+        return self.freq_spread
+
+    def set_freq_spread(self, freq_spread):
+        self.freq_spread = freq_spread
+        self.blocks_multiply_const_vxx_0.set_k((2*3.14*self.freq_spread/self.sensitivity, ))
+
+    def get_carrier_freq(self):
+        return self.carrier_freq
+
+    def set_carrier_freq(self, carrier_freq):
+        self.carrier_freq = carrier_freq
+        self.osmosdr_sink_1.set_center_freq(self.carrier_freq, 0)
+
+
+def main(top_block_cls=top_block, options=None):
+
+    qapp = Qt.QApplication(sys.argv)
+
+    tb = top_block_cls()
+    tb.start()
+    tb.show()
+
+    def quitting():
+        tb.stop()
+        tb.wait()
+    qapp.aboutToQuit.connect(quitting)
+    qapp.exec_()
+
+
+if __name__ == '__main__':
+    main()
